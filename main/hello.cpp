d2ae9eed16795f72203508582b66e91fd89735f3
Shyam Krishna Sateesh <94112101+shyamksateesh@users.noreply.github.com>
2024-03-06 12:38:42 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "webhook successfully working!\n";
+  cout << "webhook successfully working\n";
   return 0;
 }
